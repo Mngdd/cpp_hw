b276d617104d69155a56c66622585abdf5eb9349
Minegoodd <peepoopepege@gmail.com>
2025-04-20 21:49:27 +0300

    t4

---
+++ task 4/counter.hpp
@@ -0,0 +1,27 @@
+#ifndef COUNTER_H
+#define COUNTER_H
+#include <algorithm>
+#include <bits/ranges_algo.h>
+
+namespace task_4 {
+    template<class T>
+    unsigned int count_val_ = 0;
+}
+
+
+template<class D>
+struct counter {
+    counter() {
+        ++task_4::count_val_<D>;
+    }
+
+    ~counter() {
+        --task_4::count_val_<D>;
+    }
+
+    static unsigned int count() {
+        return task_4::count_val_<D>;
+    }
+};
+
+#endif //COUNTER_H
